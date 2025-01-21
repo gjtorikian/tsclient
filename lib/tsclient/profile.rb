d98291adf959e59770267a656125dd3d15a0c7dd
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2025-01-21 16:24:55 -0500

    Add some node information to `whois` response

--- lib/tsclient/profile.rb
+++ lib/tsclient/profile.rb
@@ -8,7 +8,14 @@ module Tsclient
         name: data.dig("UserProfile", "DisplayName"),
         profile_pic_url: data.dig("UserProfile", "ProfilePicURL"),
         # We assume anyone with an email address as their SSO identifier is human
-        human: data.dig("UserProfile", "LoginName").include?("@")
+        human: data.dig("UserProfile", "LoginName").include?("@"),
+        node: {
+          name: data.dig("Node", "Name"),
+          computed_name: data.dig("Node", "ComputedName"),
+          hostinfo: {
+            hostname: data.dig("Node", "Hostinfo", "Hostname"),
+          }
+        }
       )
     end
 
